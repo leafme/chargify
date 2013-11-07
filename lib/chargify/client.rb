e2ecd53021fc312809b1a817030cf09dc4d58af4
ankur@leaf.me <ankur@leaf.me>
2013-11-07 16:21:31 -0500

    don't use crack

--- lib/chargify/client.rb
+++ lib/chargify/client.rb
@@ -5,7 +5,7 @@ module Chargify
   class Parser < HTTParty::Parser
     def parse
       begin
-        Crack::JSON.parse(body) || {}
+        JSON.parse(body) || {}
       rescue => e
         raise UnexpectedResponseError, "Crack could not parse JSON. It said: #{e.message}. Chargify's raw response: #{body}"
       end
